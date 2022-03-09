bf342cae65ea0e03da8eba89a8ffe726dab5dfa8
Luan Vale <luancabral5566@outlook.com>
2022-03-09 19:03:37 -0300

    Add project files.

---
+++ ContaCorrente.ConsoleApp/ContaCorrente.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContaCorrente.ConsoleApp
+{
+    public class Movimentacoes
+    {
+        public double valorMovimentacao;
+        public string debitoOuCredito;
+    }
+
+    public class ContaCorrente
+    {
+        public int idDaConta;
+        public double saldoDaConta;
+        public bool contaEspecial;
+        public double limiteConta;
+        public int posicalAtualExtrato = 0;
+
+        public Movimentacoes[] movimentacoes;
+
+        public bool Saque(double valorSaque, out bool opcaoValida)
+        {
+
+            Movimentacoes movimentacao = new Movimentacoes();
+            movimentacao.valorMovimentacao = valorSaque;
+            movimentacao.debitoOuCredito = "Debito";
+            movimentacoes[posicalAtualExtrato] = movimentacao;
+
+
+            if (valorSaque > saldoDaConta + limiteConta)
+            {
+                opcaoValida = false;
+                return opcaoValida;
+            }
+            else
+            {
+                if (saldoDaConta > valorSaque)
+                {
+                    saldoDaConta = saldoDaConta - valorSaque;
+                    opcaoValida = true;
+                    posicalAtualExtrato++;
+                    return opcaoValida;
+                }
+                else
+                {
+                    double valorRest;
+                    valorRest = valorSaque - saldoDaConta;
+                    saldoDaConta = 0;
+                    limiteConta = limiteConta - valorRest;
+                    opcaoValida = true;
+                    posicalAtualExtrato++;
+                    return opcaoValida;
+                }
+            }
+
+        }
+
+        public double Deposito(double valorDeposito)
+        {
+            Movimentacoes movimentacao = new Movimentacoes();
+            movimentacao.valorMovimentacao = valorDeposito;
+            movimentacao.debitoOuCredito = "Credito";
+            movimentacoes[posicalAtualExtrato] = movimentacao;
+
+            posicalAtualExtrato++;
+            saldoDaConta = saldoDaConta + valorDeposito;
+            return saldoDaConta;
+        }
+
+        public void ExibirDados()
+        {
+            Console.WriteLine("==============================");
+            Console.WriteLine();
+            Console.WriteLine("Dados da conta {0}.", idDaConta);
+            Console.WriteLine();
+            Console.WriteLine("Saldo da conta: {0}.", saldoDaConta);
+
+            if (contaEspecial == true)
+            {
+                Console.WriteLine("Conta especial: Sim");
+            }
+            else
+            {
+                Console.WriteLine("Conta especial: Nao");
+            }
+
+            Console.WriteLine("Limite da conta: {0}.", limiteConta);
+            Console.WriteLine();
+        }
+
+        public void ExibirExtrato()
+        {
+            Console.WriteLine("==============================");
+            Console.WriteLine();
+            Console.WriteLine("Extrato da conta {0}.", idDaConta);
+            Console.WriteLine();
+
+            for (int i = 0; i < movimentacoes.Length; i++)
+            {
+                if (movimentacoes[i] == null)
+                {
+                    continue;
+                }
+                else
+                {
+                    Console.WriteLine("Valor movimentado: {0}", movimentacoes[i].valorMovimentacao);
+                    Console.WriteLine("Tipo de movimentacao: {0}", movimentacoes[i].debitoOuCredito);
+                    Console.WriteLine();
+                }
+            }
+
+            Console.WriteLine();
+        }
+
+        public bool TransferirPara(ContaCorrente contaPassada, double valorTransferencia, out bool opcaoValida)
+        {
+            Movimentacoes movimentacao = new Movimentacoes();
+
+            if (valorTransferencia > saldoDaConta + limiteConta)
+            {
+                opcaoValida = false;
+                return opcaoValida;
+            }
+            else
+            {
+                if (saldoDaConta > valorTransferencia)
+                {
+                    saldoDaConta = saldoDaConta - valorTransferencia;
+                    opcaoValida = true;
+
+
+                    contaPassada.saldoDaConta = contaPassada.saldoDaConta + valorTransferencia;
+
+                    movimentacao.valorMovimentacao = valorTransferencia;
+                    movimentacao.debitoOuCredito = "Debito (Transferencia)";
+                    movimentacoes[posicalAtualExtrato] = movimentacao;
+
+                    //contaPassada.movimentacoes[contaPassada.posicalAtualExtrato] = movimentacao;
+                    //contaPassada.movimentacoes[contaPassada.posicalAtualExtrato].valorMovimentacao = valorTransferencia;
+                    //contaPassada.movimentacoes[contaPassada.posicalAtualExtrato].debitoOuCredito = "Credito (Transferencia)";
+
+                    posicalAtualExtrato++;
+                    return opcaoValida;
+                }
+                else
+                {
+                    double valorRest;
+                    valorRest = valorTransferencia - saldoDaConta;
+                    saldoDaConta = 0;
+                    limiteConta = limiteConta - valorRest;
+                    opcaoValida = true;
+
+                    contaPassada.saldoDaConta = contaPassada.saldoDaConta + valorTransferencia;
+
+                    movimentacao.valorMovimentacao = valorTransferencia;
+                    movimentacao.debitoOuCredito = "Debito (Transferencia)";
+                    movimentacoes[posicalAtualExtrato] = movimentacao;
+
+                    //conta.movimentacoes[conta.posicalAtualExtrato] = movimentacao;
+                    //conta.movimentacoes[conta.posicalAtualExtrato].valorMovimentacao = valorTransferencia;
+                    //conta.movimentacoes[conta.posicalAtualExtrato].debitoOuCredito = "Credito (Transferencia)";
+
+                    posicalAtualExtrato++;
+                    return opcaoValida;
+                }
+            }
+        }
+    }
+}
---
+++ ContaCorrente.ConsoleApp/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ContaCorrente.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta1 = new ContaCorrente();
+            conta1.saldoDaConta = 1000;
+            conta1.idDaConta = 1;
+            conta1.limiteConta = 0;
+            conta1.contaEspecial = false;
+            conta1.movimentacoes = new Movimentacoes[10];
+
+            bool opcaoValida;
+
+            conta1.Saque(100, out opcaoValida);
+
+            conta1.Deposito(300);
+
+            ContaCorrente conta2 = new ContaCorrente();
+            conta2.saldoDaConta = 800;
+            conta2.idDaConta = 2;
+            conta2.limiteConta = 0;
+            conta2.contaEspecial = false;
+            conta2.movimentacoes = new Movimentacoes[10];
+
+            conta2.Saque(200, out opcaoValida);
+
+            conta1.TransferirPara(conta2, 400, out opcaoValida);
+
+            conta1.ExibirDados();
+            conta1.ExibirExtrato();
+            conta2.ExibirDados();
+            conta2.ExibirExtrato();
+
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
